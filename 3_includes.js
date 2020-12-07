28a8371a887f51f18bd1c3e4c5206c1baa67fe35
Victor Åkesson <Victorakesson@gmail.com>
2020-12-07 15:51:52 +0100

    Includes.js klar

---
+++ 3_includes.js
@@ -0,0 +1,10 @@
+const things = ['car', 'house', 'castle', 'bridge', 'tree']
+console.log(things.includes("car"));
+
+// includes kollar igenom en array och ser ifall ett elemt finns i en array om det inte gör det så returnar den false
+
+// kommer vissa sant då car finns i arrayen 
+
+console.log(things.includes("bike"));
+
+// kommer returna false då det inte finns någon bike i arrayen 
